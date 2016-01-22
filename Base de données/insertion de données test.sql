37e74a7e9f438de1fc8331178ad96a271369cd12
HDCarBoNe <dylan.gogeta@gmail.com>
2016-01-22 11:55:27 +0100

    Update

---
+++ Base de données/insertion de données test.sql
@@ -0,0 +1,16 @@
+-- Insertion de données pour les test, 1=externe, 2= demi-pentionnaire, 3=interne
+INSERT INTO `eleves`(`id`, `nom`, `prenom`, `credit`, `classe`, `regime`, `adresse`, `code_postal`, `date_naissance`, `Ville`, `info_supp`)
+VALUES
+(1,'Le Limantour','Marc','5','LT2DC','2','5 Allee des troenes','56270','1998-04-04','Ploemeur','Alergique arachide'),
+(2,'Priou','Dylan','10','LT2DC','1','7 rue le gloahec','56670','1997-03-26','Riantec',NULL),
+(3,'Baudin','Pierre','7','LT2DC','2','8 Rue Camille claudel','56700','1997-11-07','Hennebont','Alergique aux moules'),
+(4,'Eude','Herve','6','LT2DA','2','5 rue eugene delacroix','56600','1998-07-14','Lanester',NULL),
+(5,'Illien','François','2','LT2DA','2','3 Le Cruguellic','56270','1998-05-13','Ploemeur',NULL),
+(6,'Queffelec','Tristan','5','LT2DC','2','2 kerbrest izel','56520','1998-11-25','Guidel',NULL),
+(7,'Nicodeme','Stanislas','8','LT2DC','3','12 chemin de park c hoat','29170','1997-08-22','Fouesnant',NULL),
+(8,'Coquaud','Gwendal','8','LT2DA','2','6 residence penn er lann','56330','1998-10-20','Pluvigner',NULL),
+(9,'Nicolo','Quentin','8','LT2DA','3','26 rue de la vallee du loch','56390','1998-03-14','Brandivy',NULL),
+(10,'Champeau','Tom','7','LT2DA','2','17 rue edith piaf','56600','1998-09-18','Lanester',NULL),
+(11,'Coskun','Alex','10','LT2DA','1','5 rue gabrielle d arvor','56530','1998-09-18','Queven',NULL),
+(12,'Leostic','Jeremie','2','LT2DC','2','4 rte de manoir de penfrat','56530','1998-02-11','Gestel',NULL);
+
